73fe5f1deafc8a4459922525ce894510ad18c8ab
dipagalatian <dipagalatian3003@gmail.com>
2025-12-20 18:31:09 +0700

    FEAT: Create empty context with background and todo

--- context_test.go
+++ context_test.go
@@ -6,11 +6,22 @@ import (
 	"testing"
 )
 
+// Context
+// context is represent as data
+// this data can add value, cancel signal, timeout, and deadline signal
+// context have concept called parent & child
+// child context will have the same feature or functionallity like the parent context
+// context is IMMUTABLE
+// if we add value/signal cancel or more -> in the background it creates new context with that value/functionallity
+// context have func -> Deadline(), Done(), Err(), and Value() (can see and click inside the context.Background()
+
 func TestContext(t *testing.T) {
 
+	// create new context (commonly use)
 	background := context.Background()
 	fmt.Println("Background:", background)
 
+	// create new context like background (use if we dont know yet what type of context we wanna create, rarely use)
 	todo := context.TODO()
 	fmt.Println("TODO:", todo)
 	
